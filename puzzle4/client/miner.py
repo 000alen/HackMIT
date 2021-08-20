128f2e54f12224dc9e579d0522ca2dfd82f4df02
Alen Rubilar <lclc.alen@gmail.com>
2021-08-20 19:09:57 -0400

    Lorem ipsum

--- puzzle4/client/miner.py
+++ puzzle4/client/miner.py
@@ -1,23 +1,8 @@
-
-import os
 from exchange import buy, sell, get_blockchain, get_transactions, get_info
-from time import time
-from typing import List
-from blockchain import Block, Transaction, Blockchain, create_genesis_block
+from blockchain import Block, Transaction, Blockchain
 from utils import gen_uuid, post_route
 from crypto import generate_keys
 from constants import USERNAME, REWARD
-
-import datetime
-import json
-import os
-
-
-# def try_mine(block: Block):
-#     """Updates the nonce and sees if it's valid.
-#     """
-#     block.nonce += 1
-#     return block.is_valid()
 
 
 def check_if_new_block(blockchain: Blockchain):
@@ -35,26 +20,14 @@ def mine(blockchain, block: Block, start, end):
     print("\n\n" + ("-" * 40))
     print("Mining now with %i transactions." % len(block.transactions))
 
-    # start = datetime.datetime.now()
     for i in range(start, end + 1):
         if check_if_new_block(blockchain):
             return False
     
-        block.nonce += i
+        block.nonce = i
         if block.is_valid(): 
             break
 
-        # hashes_done += 1
-
-        # if hashes_done % 300000 == 0:
-        #     end = datetime.datetime.now()
-        #     seconds = (end - start).total_seconds()
-
-        #     print("Hash Rate: %i hashes/second      \r" % (300000 / seconds),)
-
-        #     if(check_if_new_block()):
-        #         return False
-        #     start = datetime.datetime.now()
     else:
         return False
 
@@ -66,59 +39,46 @@ def run_miner(public, private, blockchain, start, end):
     """Run the main miner loop.
     """
 
-    while True:
-        # Load transaction queue and blockchain from server.
-        txns: list[Transaction] = get_transactions()
-        blockchain = get_blockchain()
+    # Load transaction queue and blockchain from server.
+    txns: list[Transaction] = get_transactions()
+    blockchain = get_blockchain()
 
-        if not blockchain:
-            print("Unable to fetch blockchain")
-            return
+    if not blockchain:
+        print("Unable to fetch blockchain")
+        return
 
-        # Add reward to us yay.
-        reward = Transaction(
-            id=gen_uuid(),
-            sender="mined",
-            receiver=public,
-            value=REWARD,
-            signature=""
-        )
-        reward.sign(private)
-        txns.append(reward)
+    # Add reward to us yay.
+    reward = Transaction(
+        id=gen_uuid(),
+        sender="mined",
+        receiver=public,
+        value=REWARD,
+        signature=""
+    )
+    reward.sign(private)
+    txns.append(reward)
 
-        # Construct a new block.
-        b = Block(
-            transactions=txns,
-            previous_hash=blockchain.head.get_hash(),
-            miner=USERNAME
-        )
-
-        # Let's mine this block.
-        result = mine(b, start, end)
-
-        # Is this _the_ new block?
-        # or did the server swoop us :(
-        new_chain = get_blockchain()
-
-        if result and new_chain and new_chain.head.get_hash() == blockchain.head.get_hash():
-            # WE MINED THIS BLOCK YAY.
-            # AND WE WIN.
-            resp = post_route('add', data=b)
-            if resp and resp['success']:  # type: ignore
-                print("Block added!")
-            else:
-                print("Couldn't add block:", resp['message'])  # type: ignore
-        else:
-            print("Someone else mined the block before us :(")
-
-
-def run_sample():
-    """Testing code.
-    """
-    # Mine a sample block.
+    # Construct a new block.
     b = Block(
-        transactions=[],
-        previous_hash=create_genesis_block().get_hash()
+        transactions=txns,
+        previous_hash=blockchain.head.get_hash(),
+        miner=USERNAME
     )
 
-    mine(b)
+    # Let's mine this block.
+    result = mine(b, start, end)
+
+    # Is this _the_ new block?
+    # or did the server swoop us :(
+    new_chain = get_blockchain()
+
+    if result and new_chain and new_chain.head.get_hash() == blockchain.head.get_hash():
+        # WE MINED THIS BLOCK YAY.
+        # AND WE WIN.
+        resp = post_route('add', data=b)
+        if resp and resp['success']:  # type: ignore
+            print("Block added!")
+        else:
+            print("Couldn't add block:", resp['message'])  # type: ignore
+    else:
+        print("Someone else mined the block before us :(")
